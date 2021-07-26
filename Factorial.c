35e8752ad5b2f4ab335bc9f77b9743103feeaaa9
julcetah <87554609+julcetah@users.noreply.github.com>
2021-07-25 23:30:40 -0500

    Agrega código

---
+++ Factorial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, contador, factorial=1;
+    printf("Introduce un numero para calcular su factorial\n");
+    scanf("%i",&num);
+
+    for(contador = num; contador > 1; contador--){
+        factorial = factorial*contador;
+    }
+    printf("El factorial del numero %i es = %i \n", num, factorial);
+    getchar();
+    return 0;
+}
